f3c754a3d989c43db5699261c2471d50ff5fa4bf
Amirul Islam <amirulislam464@gmail.com>
2026-04-10 00:04:49 +0530

    Create health.js
    Add health test function

---
+++ netlify/functions/health.js
@@ -0,0 +1,10 @@
+exports.handler = async function () {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      ok: true,
+      message: "Netlify function is working"
+    })
+  };
+};
